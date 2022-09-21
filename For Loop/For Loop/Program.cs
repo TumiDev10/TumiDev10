3ae5b8f3f56814b35690a47d77d338bf1a16608f
Tumi Mashigo <tumi.mashigo15@gmail.com>
2022-09-21 20:30:21 +0200

    Add files via upload

---
+++ For Loop/For Loop/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+FizzBuzz rules:
+
+Output values from 1 to 100, one number per line.
+When the current value is divisible by 3, print the term Fizz next to the number.
+When the current value is divisible by 5, print the term Buzz next to the number.
+When the current value is divisible by both 3 and 5, print the term FizzBuzz next to the number
+*/
+
+
+for (int i = 1; i <= 100; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+    {
+        Console.WriteLine($"{i}.FizzBizz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"{i}.Fizz");
+    }
+
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine($"{i}.Bizz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+
+
+}
